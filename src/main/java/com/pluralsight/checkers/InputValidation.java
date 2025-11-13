63f5b3e0b055a8c918f1f8da23d83df6af5c8e39
notLogi <92322066+notLogi@users.noreply.github.com>
2025-11-13 13:45:13 -0500

    Changed abstract to final for Inputvalidation.

--- src/main/java/com/pluralsight/checkers/InputValidation.java
+++ src/main/java/com/pluralsight/checkers/InputValidation.java
@@ -2,7 +2,7 @@ package com.pluralsight.checkers;
 
 import java.util.Scanner;
 
-public abstract class InputValidation {
+public final class InputValidation {
     public static int parseInt(String message, Scanner scanner){
         while(true){
             try{
